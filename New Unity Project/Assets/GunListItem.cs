ccf3aae28df97fc7127c748415b0dff43565a273
edeng2 <74115529+edeng2@users.noreply.github.com>
2022-01-05 14:08:30 -0500

    new weapon images+weapons
    added compact,g36,scar(unuised)+new weapon images.

--- New Unity Project/Assets/GunListItem.cs
+++ New Unity Project/Assets/GunListItem.cs
@@ -66,7 +66,7 @@ public class GunListItem : MonoBehaviour
     }
     public void OnClickCustomize()
     {
-        if (_info.itemModels.Length <= 1)
+        if (_info.itemModels==null||_info.itemModels.Length <= 1)
         {
             return;
         }
